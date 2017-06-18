090f096de11e543494df0beafabb75e12ab14dad
kimyongyeon <toyongyeon@gmail.com>
2017-06-18 23:44:49 +0900

    바벨 클린모드 테스트 git

---
+++ hello.js
@@ -0,0 +1,19 @@
+var fnName = () => {
+	console.log("hello babel");
+	var phone = `
+		나는 누구인가?
+		나는 현재 에크마6를 공부중입니다.
+	`;
+
+	var name = `
+		i'm korean myname is kimyongyeon;
+	`;
+
+	/**
+		프리셋을 이용한 바벨 내용을 오늘 해보았습니다.
+		감사합니다. 
+	**/
+
+
+
+};
---
+++ hello_build.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var fnName = function fnName() {
+	console.log("hello babel");
+	var phone = "\n\t\t\uB098\uB294 \uB204\uAD6C\uC778\uAC00?\n\t\t\uB098\uB294 \uD604\uC7AC \uC5D0\uD06C\uB9C86\uB97C \uACF5\uBD80\uC911\uC785\uB2C8\uB2E4.\n\t";
+
+	var name = "\n\t\ti'm korean myname is kimyongyeon;\n\t";
+
+	/**
+ 	프리셋을 이용한 바벨 내용을 오늘 해보았습니다.
+ 	감사합니다. 
+ **/
+};
