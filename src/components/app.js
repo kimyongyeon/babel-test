673ef266e46c0e52e8ece0cdf920c25f8aa3a44b
kimyongyeon <toyongyeon@gmail.com>
2017-06-19 00:09:06 +0900

    웹팩 + 바벨 + 리엑트

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render(){
+
+        return (
+                <h1>안녕 리엑트 너무 좋아</h1>
+        );
+    }
+}
+
+export default App;
